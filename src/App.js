7e0f1d58070280a18b5e2f3a9ad6cf0679b39651
Obie Silva <obiesilva@gmail.com>
2023-05-14 20:28:36 -0400

    Refactoring of App.js and Home.jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
+import Home from "./pages/home/Home";
+import MapEmbed from "./components/MapEmbed";
+import Banner from "./components/Banner";
 import { useQuery } from "@apollo/client";
 import { HOME_QUERIES } from "./wpgraphql/queries";
 import Loading from "./components/reusable/Loading";
 import Error from "./components/reusable/Error";
-import Home from "./pages/home/Home";
-import MapEmbed from "./components/MapEmbed";
-import PageContent from "./pages/home/PageContent";
-import Banner from "./components/Banner";
 
 function App() {
   const { loading, error, data } = useQuery(HOME_QUERIES);
-  const pages = data?.pages?.edges || [];
-  const topBanner = data?.pages?.edges.find(({ node }) => node.home?.topBanner?.displayBanner)?.node?.home?.topBanner || null;
 
   if (loading) return <Loading />;
   if (error) return <Error error={error} />;
+
+  const topBanner = data?.pages?.edges
+    .find(({ node }) => node.home?.topBanner?.displayBanner)
+    ?.node?.home?.topBanner || null;
 
   return (
     <div className="App h-full">
@@ -26,10 +27,7 @@ function App() {
         />
       )}
       <div className="container max-w-lg mx-auto px-4">
-        <Home data={pages.map(({ node }) => node)} />
-        {pages.map(({ node }) => (
-          <PageContent node={node} />
-        ))}
+        <Home data={data} />
         <MapEmbed />
       </div>
     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
-import Header from '../../components/Header'
+import Header from "../../components/Header";
+import PageContent from "./PageContent";
 
-const Home = () => {
+const Home = ({ data }) => {
+  const pages = data?.pages?.edges || [];
+
   return (
-    <div>
+    <div className="App h-full">
       <Header />
+      <div className="container max-w-lg mx-auto px-4">
+        {pages.map(({ node }) => (
+          <PageContent key={node.id} node={node} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
