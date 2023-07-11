c9c0ad050c3e125b5803ff1206426700ebbc7f6a
Obie Silva <obiesilva@gmail.com>
2023-07-11 11:08:22 -0400

    graphQl Proxy integration

--- src/client.js
+++ src/client.js
@@ -1,7 +1,13 @@
-//ApolloClient
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_WP_GRAPHQL_URL,
+  headers: {
+    'X-Desired-Endpoint': process.env.NODE_ENV
+  }
+});
 
 export const client = new ApolloClient({
-  uri: process.env.REACT_APP_WP_GRAPHQL_URL,
+  link: httpLink,
   cache: new InMemoryCache(),
 });
