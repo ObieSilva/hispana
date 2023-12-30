6f3c4a4a0d44b1d64c31929f46ec313318deee7e
Obie Silva <obiesilva@gmail.com>
2023-12-30 18:24:27 -0500

    Update YouTube URL link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
     },
     {
       icon: RiYoutubeLine,
-      url: "https://www.youtube.com/user/hispanacfc",
+      url: "https://www.youtube.com/@hispanacfc",
     },
   ];
 
